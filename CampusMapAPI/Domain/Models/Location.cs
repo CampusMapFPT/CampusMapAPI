dfc6767a5236a46650ddf03301429308a8cfe29b
datuwu <datlt.mdc@gmail.com>
2023-03-11 22:52:15 +0700

    Init project

---
+++ CampusMapAPI/Domain/Models/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Location : BaseModel
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public string IconUrl { get; set; }
+        public string ActiveTime { get; set; }
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+        public int Floor { get; set; }
+
+    }
+}
